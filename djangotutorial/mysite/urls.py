53ad7ad38a40512730ea23fe56881ced407e95e6
Pozdnyakov3231 <101451811+Pozdnyakov3231@users.noreply.github.com>
2025-03-19 19:17:40 +0000

    Swagger

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -15,12 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets,permissions
 from polls.nocodedb import get_nocodb_data
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+schema_view = get_schema_view(
+       openapi.Info(
+           title="My API",
+           default_version='v1',
+           description="API documentation for My Django Project",
+       ),
+       public=True,
+       permission_classes=(permissions.AllowAny,),
+   )
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path('nocodb-data/', get_nocodb_data, name='nocodb_data'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),  # Список вопросов
+    path('<int:question_id>/', views.detail, name='detail'),  # Детали вопроса
+    path('<int:question_id>/results/', views.results, name='results'),  # Результаты голосования
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # Форма голосования
+    path('scooters/', views.scooters_management, name='scooters_management'),  # Управление самокатами
+    path('scooters/<int:scooter_id>/change-status/', views.change_scooter_status, name='change_scooter_status'),  # Изменение статуса самоката
 ]
